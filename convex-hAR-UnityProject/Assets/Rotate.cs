0972a1bcdd6915b640a0b6224a89dc61b5d66ccf
Aishwary Pramanik <aishwarypramanik@gmail.com>
2017-09-25 17:00:57 -0400

    Basic objects and some scripts

---
+++ convex-hAR-UnityProject/Assets/Rotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+    private float currentTime;
+    private float startTime;
+    private float rotVal;
+
+	// Use this for initialization
+	void Start () {
+        startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentTime = Time.time - startTime;
+        if (Time.deltaTime * 18 <= 360)
+        {
+            transform.Rotate(Vector3.left * Time.deltaTime * 18);
+        }
+	}
+}
---
+++ convex-hAR-UnityProject/Assets/Spawn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+    private float start;
+    private bool isSpawned;
+    private int ctr;
+
+	// Use this for initialization
+	void Start () {
+        GameObject sphere1 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere1.transform.position = new Vector3(0, 1, 0);
+
+        GameObject sphere2 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere2.transform.position = new Vector3(0, 2, -1);
+
+        start = Time.time;
+
+        isSpawned = false;
+
+        ctr = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ctr++;
+        if((Time.time - start) >= 1 && (Time.time - start) <= 2)
+            // Debug.Log("Time: "+ (Time.time - start)+" Frames: "+ctr+" Rate:"+ (1.0f / Time.deltaTime));
+        if ((Time.time - start) >= 1 && isSpawned == false)
+        {
+            
+            isSpawned = true;
+            GameObject sphere3 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere3.transform.position = new Vector3(0, 3, -2);
+        }
+    }
+}
